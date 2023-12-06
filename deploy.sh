fd7fbfa255168cf8a855d18b6d63cac5142684bd
AW <Andryaas@gmail.com>
2023-12-06 10:09:43 -0500

    improve deploy add files to .dockerignore

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ docker save -o scraping.tar aw/scraping:latest
 
 echo; echo "pushing the image to VM"
 gcloud compute scp scraping.tar scraping:/home/wavrzenczak --zone "us-central1-a" --project "waurzenczak"
+
+echo; echo "loading the image to VM"
+gcloud compute ssh --zone "us-central1-a" "scraping" --project "waurzenczak" -- \
+    'docker load -i scraping.tar && rm scraping.tar'
+
+
